26bec2b29d2113a15c480310d5868c39a2a3fd4e
GowharYousuf <gowharyousuf820@gmail.com>
2024-12-01 11:51:54 +0530

    new changes

--- pages/Components/TableFilters.jsx
+++ pages/Components/TableFilters.jsx
@@ -6,14 +6,14 @@ const TableFilters = () => {
             <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-4 mt-4 space-y-2 sm:space-y-0">
                 <button
                     className="bg-green-500 text-white px-4 py-3 rounded-md hover:bg-green-600 w-full sm:w-52 h-10"
-                    style={{ minWidth: "200px" }}
+                    style={{ minWidth: "100px" ,maxWidth:"100px"}}
                 >
                     + Add
                 </button>
                 <div className="flex flex-wrap sm:space-x-2 space-y-2 sm:space-y-0 justify-start sm:justify-end">
                     <button
                         className="bg-gray-100 text-gray-400 px-4 py-3 rounded-md w-full sm:w-52 h-10"
-                        style={{ minWidth: "200px" }}
+                        style={{ minWidth: "200px"}}
                     >
                         Check In Month
                     </button>
@@ -32,7 +32,7 @@ const TableFilters = () => {
 
                     <button
                         className="bg-gray-100 text-gray-600 px-4 py-3 rounded-md flex items-center justify-between sm:justify-center space-x-1 w-full sm:w-52 h-10"
-                        style={{ minWidth: "200px" }}
+                        style={{ minWidth: "200px"}}
                     >
                         <span>Filters</span>
                         <ChevronDownIcon className="h-5 w-5 text-gray-800" />
